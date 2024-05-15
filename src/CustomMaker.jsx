aae56da61a04d418a9e3158e27c69c621a363aaa
mutuyimana1 <amutuyimana64@gmail.com>
2024-05-15 11:55:53 +0200

    update Reame

--- src/CustomMaker.jsx
+++ src/CustomMaker.jsx
@@ -7,33 +7,32 @@ const CustomMarker = ({ position, children, nextStop }) => {
 
   const markerRef = React.useRef();
 
-  // Calculate distance and time to next stop
+ 
   const calculateDistanceAndTime = () => {
     const distanceToNextStop = calculateDistance(position, nextStop);
 
-    // Assuming average speed of 30 km/h
-    const averageSpeed = 30; // km/h
+   
+    const averageSpeed = 30; 
     const timeToNextStop = distanceToNextStop / averageSpeed;
 
     setDistance(distanceToNextStop.toFixed(2));
     setTime(timeToNextStop.toFixed(2));
   };
 
-  // Function to calculate distance between two points (in km)
   const calculateDistance = (coord1, coord2) => {
     const lat1 = coord1[0];
     const lon1 = coord1[1];
     const lat2 = coord2[0];
     const lon2 = coord2[1];
 
-    const R = 6371; // Radius of the earth in km
+    const R = 6371; 
     const dLat = deg2rad(lat2 - lat1);
     const dLon = deg2rad(lon2 - lon1);
     const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in km
+    const distance = R * c; 
     return distance;
   };
 
